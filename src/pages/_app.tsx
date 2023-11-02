322a5aeb0e0dd3d258f90dd8de95b0fd914a46e4
15Teds <teddy55johnson@gmail.com>
2023-11-02 15:41:54 -0400

    Inital Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { type AppType } from "next/dist/shared/lib/utils";
 
-import "~/styles/globals.css";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+// ^ Include your desired wallet providers...
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+import { AuthorizationProvider, CoinOpProvider } from "coinopreact";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <AuthorizationProvider>
+            <CoinOpProvider>
+              <Component {...pageProps} />
+            </CoinOpProvider>
+          </AuthorizationProvider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 };
 
 export default MyApp;
